5eaa64d1ead1bb820aa4af471ac07230738829c1
Biju Punalor <biju.punalor@gmail.com>
2026-02-08 09:26:22 +0530

    No Nonsense Neovim Client in Rust - builds for fedora Linux

---
+++ neovide/README.md
@@ -0,0 +1,23 @@
+# Unofficial release builds of Neovide
+
+[![Copr build status][copr-status-image]][copr-neovide-nightly]
+
+Build script and RPM spec for a nightly build of [Neovide][neovide]
+
+## Installing Neovide from Copr
+
+recent release builds are available from [bijumon/neovide-release][neovide-release-copr] Copr.
+Assuming you're using a recent version of Fedora, you can install it using `dnf`.
+
+```console
+# dnf copr enable bijumon/neovide-release 
+# dnf install neovide
+```
+
+[copr-neovide-nightly]: https://copr.fedorainfracloud.org/coprs/bijumon/neovide-release/package/neovide/
+[copr-status-image]: https://copr.fedorainfracloud.org/coprs/chrisbouchard/neovide-nightly/package/neovide/status_image/last_build.png
+[fedora-copr]: https://copr.fedorainfracloud.org/
+[neovide-release-copr]: https://copr.fedorainfracloud.org/coprs/bijumon/neovide-release/
+[neovide]: https://github.com/neovide/neovide
+[neovide-main-branch]: https://github.com/neovide/neovide/commits/main
+
---
+++ neovide/build.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+set -eux -o errtrace
+
+build_timestamp="$(date +'%a %b %d %T %Z %Y')"
+
+git clone 'https://github.com/Kethku/neovide.git'
+git clone 'https://github.com/chrisbouchard/neovide-nightly.git'
+
+pushd neovide
+
+    # The result here should be similar to parsing `git describe --tags HEAD`,
+    # except we don't have to worry about string parsing. The results of `git
+    # describe` are ambiguous if the tag name contains any dashes.
+
+    main_tag=$(git describe --abbrev=0 --tags HEAD)
+    main_commits=$(git rev-list --count "$main_tag..HEAD")
+    main_hash=$(git rev-parse --short HEAD)
+
+    # If the tag does contain any dashes, we need to remove them because RPM
+    # uses dash to separate the version and release.
+    safe_main_tag="${main_tag//-/.}"
+
+    # Note that we insert a "g" before the hash, just like `git describe` does.
+    # This is for backwards compatibility with our older versioning scheme that
+    # did use `git describe`.
+    version="$safe_main_tag~dev.$main_commits.g$main_hash"
+
+    git archive \
+        --prefix="neovide-$version/" \
+        --format=tar.gz \
+        --output='../neovide.tar.gz' \
+        HEAD
+
+popd
+
+pushd neovide-nightly
+
+    # We're not tagging in ths project, so we'll just count the total number of
+    # commits as the revision.
+    release="$(git rev-list --count HEAD)"
+
+    # Copy the RPM spec out.
+    cp neovide.spec ../
+
+popd
+
+# Create a companion spec file with some build-related macros.
+cat >build-macros.inc <<EOF
+%global build_release $release
+%global build_timestamp $build_timestamp
+%global build_version $version
+EOF
+
